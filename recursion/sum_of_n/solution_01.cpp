264326dc9cb8dcb0d82c1564400b1516f135c021
dick <dick@gmail.com>
2022-03-16 06:14:33 +0000

    add sum of n solution_01

---
+++ recursion/sum_of_n/solution_01.cpp
@@ -0,0 +1,21 @@
+//space complexity -- O(n)
+//time complexity -- O(n)
+
+#include <stdio.h>
+
+int sum(int n)
+{
+	if(n==0)
+	{
+		return 0;
+	}
+
+	return sum(n-1)+n;
+}
+
+int main()
+{
+	printf("%d\n",sum(5));
+
+	return 0;
+}
