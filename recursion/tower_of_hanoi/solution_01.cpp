b0f48a4054361245d5baa3499be094f38d00e19b
dick <dick@gmail.com>
2022-03-16 16:31:10 +0000

    add tower of hanoi solution 1

---
+++ recursion/tower_of_hanoi/solution_01.cpp
@@ -0,0 +1,21 @@
+//space complexity -- O(n)
+//time  complexity -- O(2^n)
+
+#include <stdio.h>
+
+void TOH(int n,int A,int B,int C)
+{
+	if(n>0)
+	{
+		TOH(n-1,A,C,B);
+		printf("(%d,%d)\n",A,C);
+		TOH(n-1,B,A,C);
+	}
+}
+
+int main()
+{
+	TOH(4,1,2,3);
+	return 0;
+}
+
