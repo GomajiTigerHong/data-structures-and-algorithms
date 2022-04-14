c30634b3fe9e7aeb0583aefa116c979c66a778d2
inseminate <tsmcakagg@gmail.com>
2022-04-14 09:30:24 +0800

    add string 001 length of array

---
+++ string/string_001_length_of_string/solution_01.cpp
@@ -0,0 +1,19 @@
+//time complexity O(n)
+
+#include <stdio.h>
+
+int main()
+{
+    char s[] = "welcome";
+
+    int i;
+
+    for(i = 0 ; s[i] != '\0' ; i++)
+    {
+
+    }
+
+    printf("length is %d",i);
+
+    return 0;
+}
